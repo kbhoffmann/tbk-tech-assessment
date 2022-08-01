eedef7487f97a54745fa0eecad628daba041316a
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-08-01 08:18:35 -0600

    Change definition of credit variable in #rates to round score down with .floor

--- app/services/rates.rb
+++ app/services/rates.rb
@@ -1,5 +1,5 @@
 module Rates
-  def credit_score(score)
+  def Rates.credit_score(score)
     if score > 800
       "excellent credit"
     elsif score >= 750 && score <= 799
@@ -7,14 +7,14 @@ module Rates
     elsif score >= 700 && score <= 749
       "good credit"
     elsif score >= 600 && score <= 699
-      "okay_credit"
+      "okay credit"
     else
       "credit needs work"
     end
   end
 
-  def rates(score)
-    credit = credit_score(score)
+  def Rates.rates(score)
+    credit = credit_score(score.floor)
     if credit == "excellent credit"
       apy = 2.8
     elsif credit == "great credit"
